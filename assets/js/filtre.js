e91b00cb983435fcef8d74939229923f0b11ee11
Ugo Nanini <2138936@csfoy.ca>
2023-05-12 09:59:54 -0400

    js filtres, correction CSS et HTML

---
+++ assets/js/filtre.js
@@ -0,0 +1,33 @@
+/**
+ * @fileoverview Filtrer selon un critère
+ */
+
+/* Variables globales */
+const fiches = document.querySelectorAll('.fiche');
+const arrBtnFiltres = document.querySelectorAll('.filtres__bouton');
+
+/* Écouteurs d'événement */
+arrBtnFiltres.forEach(function (btnFiltre) {
+    btnFiltre.addEventListener('click', filtrer);
+});
+
+console.log
+
+
+/* Fonctions */
+function filtrer() {
+    let filtreSecteur = this.dataset.secteur;
+    let filtreType = this.dataset.type;
+    // console.log(filtre);
+    fiches.forEach(function (fiche) {
+        fiche.classList.remove('selection');
+        //console.log(fiche.dataset.secteur);
+        console.log(filtreSecteur);
+        if (fiche.dataset.secteur == filtreSecteur) {
+            fiche.classList.add('selection');
+        }
+        if (fiche.dataset.type == filtreType) {
+            fiche.classList.add('selection')
+        }
+    });
+}
